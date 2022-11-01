06472f9e4f9ccb6ea4107fe89fd10b03333e84a8
nickforster <nick_forster@icloud.com>
2022-11-01 10:08:02 +0100

    server setup

--- api/main.go
+++ api/main.go
@@ -1,4 +1,32 @@
 package main
 
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/rs/cors"
+	"log"
+	"net/http"
+)
+
+var db, err = sql.Open("mysql", "username:password@tcp(127.0.0.1:3306)/management_System")
+
+const port = "3000"
+
 func main() {
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", start)
+
+	handler := cors.AllowAll().Handler(mux)
+
+	log.Println("Listening for connections on port: ", port)
+	log.Fatal(http.ListenAndServe(":"+port, handler))
+}
+
+func start(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("welcome"))
 }
