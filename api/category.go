08b5425c5bbfda114a82e30c89ee5990344c8875
nickforster <nick_forster@icloud.com>
2022-11-19 14:04:32 +0100

    added category.go file with SQL functions

---
+++ api/category.go
@@ -0,0 +1,55 @@
+package main
+
+type Category struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+func insertCategory(name string) error {
+	insert, err := db.Query("INSERT INTO categories VALUES(null, ?);", name)
+	err = insert.Close()
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+func deleteCategory(id int) error {
+	rows, err := db.Query("DELETE FROM categories WHERE id=?", id)
+	err = rows.Close()
+	if err != nil {
+		return err
+	}
+
+	return err
+}
+
+func updateCategory(id int, name string) error {
+	rows, err := db.Query("UPDATE categories set name=? WHERE id=?", name, id)
+	err = rows.Close()
+	if err != nil {
+		return err
+	}
+	return err
+}
+
+func getAllCategories() ([]Category, error) {
+	var categories []Category
+	rows, err := db.Query("SELECT * from categories")
+	if err != nil {
+		return categories, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var category Category
+		if err := rows.Scan(&category.Id, &category.Name); err != nil {
+			return categories, err
+		} else {
+			categories = append(categories, category)
+		}
+	}
+
+	return categories, err
+}
