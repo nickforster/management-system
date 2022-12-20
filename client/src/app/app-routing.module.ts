376268ec3f784108bf85652350b0c4474c3a0f25
nickforster <nick_forster@icloud.com>
2022-12-20 10:11:43 +0100

    added app component

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,8 +4,10 @@ import {HomeComponent} from "./home/home.component";
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
 import {SettingsComponent} from "./settings/settings.component";
+import {AppComponent} from "./app/app.component";
 
 const routes: Routes = [
+  { path: 'app', component: AppComponent},
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'settings', component: SettingsComponent },
