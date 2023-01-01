5bc8aca66523d4456dd97ce0ecf546d66f21bcb1
nickforster <nick_forster@icloud.com>
2023-01-01 19:50:19 +0100

    created user service

---
+++ client/src/app/services/user.service.ts
@@ -0,0 +1,40 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import jwtDecode from "jwt-decode";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  API_URL = 'http://localhost:3000';
+  TOKEN_KEY = 'token';
+  userID: number = -1
+  headers = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Cache-Control': 'no-cache',
+    })
+  };
+
+  constructor(private http: HttpClient) {
+  }
+
+  readToken() {
+    let token = localStorage.getItem(this.TOKEN_KEY)
+    if (token != null) {
+      let decoded = jwtDecode(token)
+      // @ts-ignore
+      this.userID = decoded.id
+    }
+  }
+
+  getUser() {
+    this.readToken()
+    return this.http.post(this.API_URL + '/getUser', {id: this.userID}, this.headers)
+  }
+
+  updateUser(username: string, email: string, password: string) {
+    this.readToken()
+    this.http.post(this.API_URL + '/updateUser', {id: this.userID, username, email, password}, this.headers).subscribe();
+  }
+}
