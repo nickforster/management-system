1312eda87f425d721f30afc35b936e4afdb9ae25
nickforster <nick_forster@icloud.com>
2023-01-16 17:38:30 +0100

    updated register site and added errors

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {faUser, faKey, faEnvelope, faEye, faEyeSlash} from '@fortawesome/free-solid-svg-icons';
+import {faUser, faKey, faEnvelope, faEye, faEyeSlash, faSpinner} from '@fortawesome/free-solid-svg-icons';
 import {HttpClient} from "@angular/common/http";
 import {Router} from "@angular/router";
 import {AuthenticationService} from "../services/authentication.service";
@@ -13,25 +13,41 @@ export class RegisterComponent implements OnInit {
   username = ''
   email = ''
   password = ''
-
-  error = false
+  registering = false
+  error = ''
 
   faUser = faUser;
   faKey = faKey;
   faEnvelope = faEnvelope;
   faEye = faEye;
+  faSpinner = faSpinner
 
   constructor(private httpClient: HttpClient,
               public router: Router,
               private authenticationService: AuthenticationService
-  ) { }
+  ) {
+    document.addEventListener('keydown', (e) => {
+      if (e.key === 'Enter') {
+        this.register()
+      }
+    })
+  }
 
   ngOnInit(): void {
   }
 
   register() {
     if (this.username != '' && this.email != '' && this.password != '') {
-      this.authenticationService.register(this.username, this.email, this.password)
+      this.registering = true
+      this.authenticationService.register(this.username, this.email, this.password).subscribe(
+        (res: any) => {
+          localStorage.setItem('token', res.token);
+          this.router.navigateByUrl('/');
+        }, (err) => {
+          this.error = err.error
+          this.registering = false
+        }
+      );
     }
   }
 
