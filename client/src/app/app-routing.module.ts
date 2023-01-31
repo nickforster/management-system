534d6329a73d58c76072d904b84db8eddec6a1b1
nickforster <nick_forster@icloud.com>
2023-01-31 09:51:44 +0100

    updated footer links and style

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,8 +4,12 @@ import {HomeComponent} from "./home/home.component";
 import {LoginComponent} from "./login/login.component";
 import {RegisterComponent} from "./register/register.component";
 import {SettingsComponent} from "./settings/settings.component";
+import {NoticeComponent} from "./legal/notice/notice.component";
+import {PrivacyComponent} from "./legal/privacy/privacy.component";
 
 const routes: Routes = [
+  { path: 'legalNotice', component: NoticeComponent},
+  { path: 'dataPrivacy', component: PrivacyComponent},
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'settings', component: SettingsComponent },
