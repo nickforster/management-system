d2c9bc3b840dbed596cc8e96b9274e558872ea83
nickforster <nick_forster@icloud.com>
2023-02-02 23:33:05 +0100

    filled legal components with content

--- client/src/app/legal/notice/notice.component.ts
+++ client/src/app/legal/notice/notice.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {faHouse} from '@fortawesome/free-solid-svg-icons';
+import {AuthenticationService} from "../../services/authentication.service";
 
 @Component({
   selector: 'app-notice',
   templateUrl: './notice.component.html',
-  styleUrls: ['./notice.component.css']
+  styleUrls: ['./notice.component.css', '../legal.components.css']
 })
 export class NoticeComponent implements OnInit {
+  faHouse = faHouse
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService) {
+  }
 
   ngOnInit(): void {
+    this.authenticationService.authorise()
   }
 
 }
--- client/src/app/legal/privacy/privacy.component.ts
+++ client/src/app/legal/privacy/privacy.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {faHouse} from '@fortawesome/free-solid-svg-icons';
+import {AuthenticationService} from "../../services/authentication.service";
 
 @Component({
   selector: 'app-privacy',
   templateUrl: './privacy.component.html',
-  styleUrls: ['./privacy.component.css']
+  styleUrls: ['./privacy.component.css', '../legal.components.css']
 })
 export class PrivacyComponent implements OnInit {
+  faHouse = faHouse
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService) {
+  }
 
   ngOnInit(): void {
+    this.authenticationService.authorise()
   }
 
 }
