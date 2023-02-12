17428bd1f67b69dd4bc9afbb45e0fc750ad07fe4
nickforster <nick_forster@icloud.com>
2023-02-12 23:50:29 +0100

    updated contact component

--- client/src/app/infos/contact/contact.component.ts
+++ client/src/app/infos/contact/contact.component.ts
@@ -16,7 +16,7 @@ export class ContactComponent implements OnInit {
   faQuoteRight = faQuoteRight
   faMessage = faMessage
 
-  mail: Mail = {username: "", email: "", subject: "", message: ""}
+  mail: Mail = {name: "", from: "", to: "", subject: "", message: ""}
 
   error = ''
   success = ''
@@ -30,26 +30,44 @@ export class ContactComponent implements OnInit {
   }
 
   send() {
-
+    if (this.mail.name === '' || this.mail.from === '' || this.mail.subject === '' || this.mail.message === '') {
+      this.error = "Please fill in all fields"
+      this.success = ""
+      return
+    }
+    if (!new RegExp("^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-]+)(\\.[a-zA-Z]{2,5}){1,2}$").test(this.mail.from)) {
+      this.error = "Pleas use a valid email"
+      this.success = ""
+      return
+    }
+    this.contactService.sendMail(this.mail).subscribe(() => {
+      this.error = ""
+      this.success = "The mail has been sent"
+      this.clear()
+    }, (err) => {
+      this.success = ""
+      this.error = err.error
+    })
   }
 
   clear() {
-    this.mail = {username: "", email: "", subject: "", message: ""}
+    this.mail = {name: "", from: "", to: "nickforster23@gmail.com", subject: "", message: ""}
     this.error = ""
   }
 
   loadData() {
     this.userService.getUser().subscribe(res => {
       this.mail = JSON.parse(JSON.stringify(res))
-      this.mail.username = this.mail.username.charAt(0).toUpperCase() + this.mail.username.slice(1)
+      this.mail.name = this.mail.name.charAt(0).toUpperCase() + this.mail.name.slice(1)
     })
   }
 
 }
 
 interface Mail {
-  username: string;
-  email: string;
+  name: string;
+  from: string;
+  to: string;
   subject: string;
   message: string;
 }
