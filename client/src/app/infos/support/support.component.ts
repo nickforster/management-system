31430418a68dc1b41a894dfb8a867c427eddd8b0
nickforster <nick_forster@icloud.com>
2023-02-15 17:30:34 +0100

    added content to support component

--- client/src/app/infos/support/support.component.ts
+++ client/src/app/infos/support/support.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {faHouse} from '@fortawesome/free-solid-svg-icons';
+import {AuthenticationService} from "../../services/authentication.service";
 
 @Component({
   selector: 'app-support',
   templateUrl: './support.component.html',
-  styleUrls: ['./support.component.css']
+  styleUrls: ['./support.component.css', '../infos.components.css']
 })
 export class SupportComponent implements OnInit {
+  faHouse = faHouse
 
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService) {
+  }
 
   ngOnInit(): void {
+    this.authenticationService.authorise()
   }
 
 }
