d0ec60ee5f41883b6acde4f2506f33aea48a36fd
nickforster <nick_forster@icloud.com>
2023-02-20 17:17:36 +0100

    started home component

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthenticationService} from "../services/authentication.service";
-import {faGear} from '@fortawesome/free-solid-svg-icons';
+import {faGear, faPlay, faPlus, faPenToSquare, faMoneyBill, faClockRotateLeft} from '@fortawesome/free-solid-svg-icons';
+import {TableService} from "../services/table.service";
 
 @Component({
   selector: 'app-home',
@@ -9,9 +10,16 @@ import {faGear} from '@fortawesome/free-solid-svg-icons';
 })
 export class HomeComponent implements OnInit {
   faGear = faGear
+  faPlay = faPlay
+  faPlus = faPlus
+  faPenToSquare = faPenToSquare
+  faMoneyBill = faMoneyBill
+  faClockRotateLeft = faClockRotateLeft
   name = "User"
+  active: boolean[] = []
+  tables: Table[] = []
 
-  constructor(private authenticationService: AuthenticationService) {
+  constructor(private authenticationService: AuthenticationService, private tableService: TableService) {
     let token = localStorage.getItem("token")
     if (token !== null) {
       this.name = JSON.parse(atob(token.split('.')[1])).username
@@ -20,6 +28,21 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.authenticationService.authorise()
+    this.loadData()
   }
 
+  loadData() {
+    this.tableService.getAllTables().subscribe(res => {
+      this.tables = JSON.parse(JSON.stringify(res))
+      for (let i = 0; i < this.tables.length; i++) {
+        this.active[i] = true // TODO make database request to get correct value
+      }
+    })
+  }
+}
+
+interface Table {
+  id: number;
+  name: string;
+  amountPeople: number;
 }
