768130bd134c0dfd3dbf51e4b0085408861611ec
nickforster <nick_forster@icloud.com>
2023-02-22 15:55:19 +0100

    updated home component

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthenticationService} from "../services/authentication.service";
-import {faGear, faPlay, faPlus, faPenToSquare, faMoneyBill, faClockRotateLeft} from '@fortawesome/free-solid-svg-icons';
+import {faClockRotateLeft, faGear, faMoneyBill, faPenToSquare, faPlay, faPlus} from '@fortawesome/free-solid-svg-icons';
 import {TableService} from "../services/table.service";
 
 @Component({
@@ -35,9 +35,33 @@ export class HomeComponent implements OnInit {
     this.tableService.getAllTables().subscribe(res => {
       this.tables = JSON.parse(JSON.stringify(res))
       for (let i = 0; i < this.tables.length; i++) {
-        this.active[i] = true // TODO make database request to get correct value
+        this.active[i] = false // TODO make database request to get correct value
       }
     })
+  }
+
+  start(index: number) {
+    this.active[index] = true
+    // TODO create order in database and set table to active
+  }
+
+  history(index: number) {
+    // TODO make request for history of this table
+    // TODO show the history of this table
+  }
+
+  add(index: number) {
+    // TODO show modal to add food
+  }
+
+  edit(index: number) {
+    // TODO show modal with foods of current order to edit
+  }
+
+  bill(index: number) {
+    // TODO show bill of order and make payment etc.
+    // TODO finish order in database and add it to history
+    this.active[index] = false
   }
 }
 
