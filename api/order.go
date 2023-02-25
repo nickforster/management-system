7605e2879ffc10e8b254dd35a7d3ba437e7429e1
nickforster <nick_forster@icloud.com>
2023-02-25 15:09:51 +0100

    added getAllActiveOrders method

--- api/order.go
+++ api/order.go
@@ -26,8 +26,24 @@ func getAllOrders(userID int) ([]Order, error) {
 	return orders, err
 }
 
-func getAllActiveOrders(userID int) {
+func getAllActiveOrders(userID int) ([]Order, error) {
+	var orders []Order
+	rows, err := db.Query("SELECT order_id, table_id, complete FROM orders LEFT JOIN tables ON orders.table_id=tables.table_id WHERE user_id = ? AND complete=false", userID)
+	if err != nil {
+		return orders, err
+	}
+	defer rows.Close()
 
+	for rows.Next() {
+		var order Order
+		if err := rows.Scan(&order.Id, &order.TableID, &order.Complete); err != nil {
+			return orders, err
+		} else {
+			orders = append(orders, order)
+		}
+	}
+
+	return orders, err
 }
 
 func createOrderInDB(tableID int, complete bool, userID int) {
