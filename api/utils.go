2be2864bb040ddcb78098d696a4e82ef06c09207
nickforster <nick_forster@icloud.com>
2023-03-08 20:43:35 +0100

    added structs and readBody methods

--- api/utils.go
+++ api/utils.go
@@ -17,6 +17,16 @@ type Mail struct {
 	Message string `json:"message"`
 }
 
+type UserID struct {
+	UserID int `json:"userId"`
+}
+
+type OrderFood struct {
+	Id      int `json:"id"`
+	OrderID int `json:"orderId"`
+	FoodID  int `json:"foodId"`
+}
+
 var jwtKey = []byte("TdbDXoW^5TQeNVm2fq5Qf6jbG*z^GsrZ^Fp5bcBE!tXJ7rvi37Gm5nrxz6WWiVFqseA^2x!PLs7uP*d!H8ZiPsYgs$Wju%M&R@62hzCUGTsbNoVX3Uuc3fRrV468@$o9")
 
 func readUserBody(w http.ResponseWriter, r *http.Request, errorMessage string) User {
@@ -87,9 +97,60 @@ func readTableBody(w http.ResponseWriter, r *http.Request, errorMessage string) 
 	return data
 }
 
+func readOrderBody(w http.ResponseWriter, r *http.Request, errorMessage string) Order {
+	body, err := ioutil.ReadAll(r.Body)
+	data := Order{} // struct from order.go file
+	if err != nil {
+		http.Error(w, errorMessage, http.StatusUnauthorized)
+		panic(err.Error())
+	}
+
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		http.Error(w, errorMessage, http.StatusInternalServerError)
+		panic(err.Error())
+	}
+
+	return data
+}
+
+func readOrderFoodBody(w http.ResponseWriter, r *http.Request, errorMessage string) OrderFood {
+	body, err := ioutil.ReadAll(r.Body)
+	data := OrderFood{}
+	if err != nil {
+		http.Error(w, errorMessage, http.StatusUnauthorized)
+		panic(err.Error())
+	}
+
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		http.Error(w, errorMessage, http.StatusInternalServerError)
+		panic(err.Error())
+	}
+
+	return data
+}
+
 func readMailBody(w http.ResponseWriter, r *http.Request, errorMessage string) Mail {
 	body, err := ioutil.ReadAll(r.Body)
 	data := Mail{} // struct from utils.go file
+	if err != nil {
+		http.Error(w, errorMessage, http.StatusUnauthorized)
+		panic(err.Error())
+	}
+
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		http.Error(w, errorMessage, http.StatusInternalServerError)
+		panic(err.Error())
+	}
+
+	return data
+}
+
+func readBody(w http.ResponseWriter, r *http.Request, errorMessage string) UserID {
+	body, err := ioutil.ReadAll(r.Body)
+	data := UserID{} // struct from utils.go file
 	if err != nil {
 		http.Error(w, errorMessage, http.StatusUnauthorized)
 		panic(err.Error())
